0d4b5dd16b6a216691f1e0ef58ea26c5bb1116a1
rfloyd01 <lynrdskynrd05@gmail.com>
2022-02-09 18:20:03 -0500

    added routing for registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { RegisterComponent } from './components/register/register.component';
 import { TestComponent } from './components/test/test.component';
 
 const routes: Routes = [{
@@ -10,6 +11,9 @@ const routes: Routes = [{
 }, {
   path:"login",
   component:LoginComponent
+}, {
+  path:"register",
+  component:RegisterComponent
 }
 ];
 

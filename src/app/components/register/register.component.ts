e58e2f924cc292a882ea56b1fa20406a51342a61
rfloyd01 <lynrdskynrd05@gmail.com>
2022-02-13 10:33:20 -0500

    can now register a user and have it persist in database. added some basic validation on front end for user creation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { CurrentUserService } from 'src/app/services/current-user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  //user:User = new User(0, "", "", "", "", ""); //is there a better way for default initialization?
+  firstName:string = "";
+  username:string = "";
+  email:string = "";
+  password:string = "";
+  repeatPassword:string = "";
+  checked:boolean = false;
+
+  constructor(private userService:CurrentUserService) { }
 
   ngOnInit(): void {
+  }
+
+  registerUser(): void {
+    //make sure no fields are blank, then create a user and send to the backend
+
+    if (this.firstName == "" || this.username == "" || this.email == "" || this.password == "" || this.repeatPassword == "") {
+      alert("Must fill out all fields.");
+    }
+    else if (this.password != this.repeatPassword) {
+      alert("passwords entered don't match.")
+    }
+    else if (!this.checked){
+      alert("You must agree to the terms of service.")
+    }
+    else {
+      //first create a new user object
+      let user:User = new User(0, this.username, this.password, this.email, this.firstName, "");
+
+      //we only have a single input box for name. Need a little function to split up first name and last name.
+      //if no last name was given the field will be left blank (last name can be blank in the database but firstname can't)
+      let nameArray:string[] = this.firstName.split(" ");
+
+      user.firstName = nameArray[0];
+      if (nameArray.length > 1) {
+        //There can be multiple last names, all get rolled into a single last name
+        let lastName:string = "";
+        for (let i:number = 1; i < nameArray.length; i++) {
+          lastName += nameArray[i];
+          lastName += " ";
+        }
+        lastName = lastName.slice(0, -1); //remove the trailing " " character
+        user.lastName = lastName;
+      }
+      console.log(user);
+
+      //create new user here using the userService
+      this.userService.createUser(user).subscribe(
+        (resposne:User)=>{
+
+        }
+      )
+    }
+
   }
 
 }
--- src/app/services/current-user.service.ts
+++ src/app/services/current-user.service.ts
@@ -12,7 +12,7 @@ export class CurrentUserService {
   //this service will hold data about the currently signed in user and will be able to be accessed from 
   //the necessary components
   currentUser:User;
-  url:string = "http://localhost:8083/"; //this is currently set for Bobby's EC2
+  url:string = "http://localhost:8083/";
 
   constructor(private http:HttpClient) { 
     this.currentUser  = new User(0, "", "", "", "", ""); //starts off as a blank user upon instantiation
@@ -33,6 +33,10 @@ export class CurrentUserService {
     //this function is used when logging out. It erases cached information about the user that was previously
     //logged in
     this.currentUser  = new User(0, "", "", "", "", "");
+  }
+
+  createUser(user:User):Observable<User> {
+    return this.http.post(this.url + "users", user) as Observable<User>;
   }
 
   //TODO: Add some more functions at some point which will allow the user to change some of their information

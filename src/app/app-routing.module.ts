ba565244d8f2d20cf78ed5a3397705f40e0476cc
idanzigm <idanzigm@gmail.com>
2022-02-19 19:04:55 -0500

    testing single-player game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { TestComponent } from './components/test/test.component';
 import { GameModeComponent } from './components/game-mode/game-mode.component';
 import { PracticeModeComponent } from './components/practice-mode/practice-mode.component';
+import { SinglePlayerComponent } from './components/single-player/single-player.component';
 
 const routes: Routes = [{
   path:"",
@@ -25,6 +26,9 @@ const routes: Routes = [{
 }, {
   path:"practice-mode",
   component:PracticeModeComponent
+}, {
+  path:"single-player", 
+  component:SinglePlayerComponent
 }
 ];
 

b62c5bce3efb21520147f8f19b5da25b9c38fe6b
rfloyd01 <lynrdskynrd05@gmail.com>
2022-02-20 15:14:17 -0500

    messed around with potential main game functionality in test component

--- src/app/components/update-user/update-user.component.ts
+++ src/app/components/update-user/update-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { CurrentUserService } from 'src/app/services/current-user.service';
 
 @Component({
   selector: 'app-update-user',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private currentUserService:CurrentUserService) { }
 
   ngOnInit(): void {
   }

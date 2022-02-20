d8e839e1ddd963027f9b034e4138dcaa75c24d13
idanzigm <idanzigm@gmail.com>
2022-02-19 19:00:27 -0500

    testing main game

--- src/app/models/answer.model.ts
+++ src/app/models/answer.model.ts
@@ -1,2 +1,26 @@
 export class Answer {
+
+    public id:number; 
+    public givenAnswer:string; 
+
+    constructor(id:number, givenAnswer:string) {
+        this.id = id; 
+        this.givenAnswer = givenAnswer
+    }
+
+    setId(id:number) {
+        this.id = id; 
+    }
+
+    getId() {
+        return this.id; 
+    }
+
+    setGivenAnswer(givenAnswer:string) {
+        this.givenAnswer = givenAnswer; 
+    }
+
+    getGivenAnswer() {
+        return this.givenAnswer; 
+    }
 }
